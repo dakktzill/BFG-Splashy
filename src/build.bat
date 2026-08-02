812049c26390cf1f4727b2e2f39875a941520ba0
Dakktyl <126857288+TheSilliestGeese@users.noreply.github.com>
2026-08-02 22:26:50 +0100

    shrink compiled filesize

--- src/build.bat
+++ src/build.bat
@@ -6,8 +6,8 @@ mkdir "../builds"
 mkdir "../builds/linux"
 mkdir "../builds/windows"
 
-go build -o ../builds/linux/server ./cmd/server
-go build -o ../builds/linux/setup ./cmd/setup
+go build -o ../builds/linux/server ./cmd/server -trimpath
+go build -o ../builds/linux/setup ./cmd/setup -trimpath
 
 go build -o ../builds/windows/server.exe ./cmd/server/
 go build -o ../builds/windows/setup.exe ./cmd/setup/
