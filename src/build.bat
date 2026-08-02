b223b2b35a21736808d9570223658dbbff421d8a
Dakktyl <126857288+TheSilliestGeese@users.noreply.github.com>
2026-08-02 22:10:29 +0100

    fix byuilding because im stupid

--- src/build.bat
+++ src/build.bat
@@ -13,19 +13,17 @@ go build -o ../builds/windows/server.exe ./cmd/server/
 go build -o ../builds/windows/setup.exe ./cmd/setup/
 
 xcopy "..\db" "..\builds\linux\db" /E /I
-xcopy "..\dlc" "..\builds\linux\db" /E /I
+xcopy "..\dlc" "..\builds\linux\dlc" /E /I
 copy "..\README.md" "..\builds\linux\README.md"
 copy "..\LICENSE" "..\builds\linux\LICENSE"
 copy "..\constants.json" "..\builds\linux\constants.json"
 
-xcopy "..\dlc" "..\builds\windows\db" /E /I
+xcopy "..\dlc" "..\builds\windows\dlc" /E /I
 copy "..\README.md" "..\builds\windows\README.md"
 copy "..\LICENSE" "..\builds\windows\LICENSE"
 copy "..\constants.json" "..\builds\windows\constants.json"
 
 powershell -Command "Compress-Archive -Path '..\builds\linux\' -DestinationPath '..\builds\linux.zip'"
 powershell -Command "Compress-Archive -Path '..\builds\windows\' -DestinationPath '..\builds\windows.zip'"
-
-echo Build Complete!
 
 pause
